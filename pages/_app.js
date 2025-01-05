3cd16f9277948f1414969cd5719e29a20fbbd3c8
Barbara <info@nonterritorial.foundation>
2025-01-05 18:14:22 +0100

    SEO fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,32 @@
 import Head from "next/head";
 import "../styles/style.css";
-import Script from "next/script";
 
 export default function MyApp({ Component, pageProps }) {
     return (
         <>
             <Head>
+                {/* Favicon */}
+                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+                <link rel="manifest" href="/site.webmanifest" />
+
+                {/* Global Site Tag (gtag.js) - Google Analytics */}
                 <script
-                    type="application/ld+json"
+                    async
+                    src={`https://www.googletagmanager.com/gtag/js?id=G-NBVLCF33EK`}
+                ></script>
+                <script
                     dangerouslySetInnerHTML={{
-                        __html: JSON.stringify({
-                            "@context": "https://schema.org",
-                            "@type": "Organization",
-                            "name": "PagesMeter",
-                            "url": "https://www.PagesMeter.com",
-                            "logo": "https://www.PagesMeter.com/logo.png",
-                        }),
+                        __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', 'G-NBVLCF33EK', {
+                                page_path: window.location.pathname,
+                            });
+                        `,
                     }}
                 />
             </Head>
@@ -23,3 +34,4 @@ export default function MyApp({ Component, pageProps }) {
         </>
     );
 }
+
