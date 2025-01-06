988c1d74da326a97c4a0c0fa32545a7bb3ad7095
Barbara <info@nonterritorial.foundation>
2025-01-06 11:00:02 +0100

    favicon

--- pages/_document.js
+++ pages/_document.js
@@ -6,9 +6,7 @@ export default function Document() {
             <Head>
                 {/* Favicon */}
                 <link rel="icon" href="/favicon.ico" />
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-                <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+                <link rel="apple-icon" href="/apple-icon.png" />
                 <link rel="manifest" href="/site.webmanifest" />
 
                 {/* Preload Fonts */}
