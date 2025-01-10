98380e448c107b24a91c8f643090a24d3918a0b4
Barbara <info@nonterritorial.foundation>
2025-01-10 12:49:02 +0100

    api fix

--- pages/api/trips.js
+++ pages/api/trips.js
@@ -3,6 +3,7 @@ export default async function handler(req, res) {
         try {
             // Fetch the JSON file from the public folder
             const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'}/trips.json`);
+
             const trips = await response.json();
             res.status(200).json(trips);
         } catch (error) {
